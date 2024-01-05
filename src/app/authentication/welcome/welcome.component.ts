c6f96d6f90b28582d99c701559d4fe219955fc8d
BradMotech <Bradley@motech.dev>
2024-01-05 13:30:13 +0200

    some more core final files

---
+++ src/app/authentication/welcome/welcome.component.ts
@@ -0,0 +1,34 @@
+import { Routes, RouterModule, Router } from '@angular/router';
+import { Component, OnInit,Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent {
+  spinner: boolean = false;
+  isBrowser: boolean;
+  isServer: boolean;
+  constructor(private route: Router, @Inject(PLATFORM_ID) private platformId: any)
+  {
+    // console.log(platformId)
+    this.isBrowser = isPlatformBrowser(platformId);
+    this.isServer = isPlatformServer(platformId);
+  }
+
+  ngOnInit(): void
+  {
+    console.log(this.isBrowser)
+    this.spinner = true;
+    setTimeout(() => {
+      this.spinner = false;
+      // Swal.fire('Yikes!', 'Something went wrong!', 'error')
+    }, 3000);
+  }
+
+  proceed(): void {
+    this.route.navigate(['authentication/login']);
+  }
+}
