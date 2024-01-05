dc32541236d0c685f1a6f0188a7dfb3f06dfac2b
BradMotech <Bradley@motech.dev>
2024-01-05 13:25:49 +0200

    assets

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'e-health-3eda1',
+    appId: '1:544211878229:web:7851d2d4dbc7e6374eafdc',
+    databaseURL: 'https://e-health-3eda1-default-rtdb.firebaseio.com',
+    storageBucket: 'e-health-3eda1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDtxZple_3xBBV5UG5nFKtnAKkOE1QlAis',
+    authDomain: 'e-health-3eda1.firebaseapp.com',
+    messagingSenderId: '544211878229',
+    measurementId: 'G-WK94J3CD3K',
+  },
+  production: true,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'e-health-3eda1',
+    appId: '1:544211878229:web:7851d2d4dbc7e6374eafdc',
+    databaseURL: 'https://e-health-3eda1-default-rtdb.firebaseio.com',
+    storageBucket: 'e-health-3eda1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDtxZple_3xBBV5UG5nFKtnAKkOE1QlAis',
+    authDomain: 'e-health-3eda1.firebaseapp.com',
+    messagingSenderId: '544211878229',
+    measurementId: 'G-WK94J3CD3K',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
